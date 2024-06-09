e6e58833b12093300778b2e5a14d07d90b0a9216
Kirito <2493381254@qq.com>
2024-06-09 20:58:12 +0800

    add: 使用hmap模式测试snowfalke生成短链接成功

--- backend/pkg/handler/shortUrl/shortUrl.go
+++ backend/pkg/handler/shortUrl/shortUrl.go
@@ -1,3 +1,13 @@
 package shortUrl
 
+import "github.com/Kiritoabc/short-link/backend/pkg/utils/snowflake"
+
 type shortURLHandler struct{}
+
+func (s *shortURLHandler) GetShortLink(url string) (string, error) {
+	// snowflake
+	id := snowflake.Snowflake.Generate()
+	idStr := id.String()
+	println(idStr)
+	return idStr, nil
+}
